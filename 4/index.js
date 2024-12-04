a479748b5b115a42a6163a12076d9df42fec8cd4
AbelAnaya <abelanaya97@gmail.com>
2024-12-04 01:41:32 +0100

    feat(sw): Add day 4 challenge. 5 stars

--- 4/index.js
+++ 4/index.js
@@ -1,3 +1,18 @@
-console.log('hello world')
+function createXmasTree(height, ornament) {
+    let BODY = ''
 
-let a = [1, 2, 3, 4, 5]
+    for (let i = 0; i < height; i++) {
+        const LINE_UNDERSCORES = '_'.repeat(height - 1 - i)
+        const LINE = LINE_UNDERSCORES + ornament.repeat(1 + 2 * i) + LINE_UNDERSCORES + '\n'
+        BODY += LINE
+    }
+
+    const FOOTER_UNDERSCORES = '_'.repeat(height - 1)
+    const FOOTER_LINE = FOOTER_UNDERSCORES + '#' + FOOTER_UNDERSCORES
+    const FOOTER = FOOTER_LINE + '\n' + FOOTER_LINE
+
+    const OUTPUT = BODY + FOOTER
+    return OUTPUT
+}
+
+console.log(createXmasTree(6, '*'))
