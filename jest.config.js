f2b2d5e0a0b5dd912bb12c7cc510e54eabd2dbd0
AbelAnaya <abelanaya97@gmail.com>
2024-12-04 18:34:04 +0100

    feat(sw): Improve jest integration

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    testEnvironment: 'node',
-    testRegex: '/(?!template/).*\\.test\\.ts$',
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-}
