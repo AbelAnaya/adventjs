924941c51c824172c725c10477017e4e90d1290b
AbelAnaya <abelanaya97@gmail.com>
2024-12-09 18:46:39 +0100

    Add challenge 9. 5 star

--- src/9/index.ts
+++ src/9/index.ts
@@ -1,48 +1,26 @@
-export type Board = Array<String>
+export type Board = String[]
 type Movement = 'U' | 'D' | 'R' | 'L'
 type Result = 'none' | 'crash' | 'eat'
 
 export default function moveTrain(board: Board, mov: Movement): Result {
-    function checkStatus(trainPosition: Array<number>): Result {
-        if (trainPosition[0] < 0 || trainPosition[1] < 0) return 'crash'
-        if (trainPosition[0] >= board.length || trainPosition[1] >= board[0].length) return 'crash'
-        const positionChar = board[trainPosition[0]][trainPosition[1]]
-
-        if (positionChar === '*') return 'eat'
-        if (positionChar === '@' || positionChar === 'o') return 'crash'
-
-        return 'none'
+    const movActions = {
+        U: -board[0].length - 1,
+        D: +board[0].length + 1,
+        L: -1,
+        R: +1,
     }
 
-    let trainPosition = [0, 0]
-
-    board.forEach((row, index) => {
-        if (row.indexOf('@') !== -1) {
-            trainPosition = [index, row.indexOf('@')]
-        }
-    })
-
-    switch (mov) {
-        case 'U':
-            trainPosition[0]--
-            break
-
-        case 'D':
-            trainPosition[0]++
-            break
-
-        case 'L':
-            trainPosition[1]--
-            break
-
-        case 'R':
-            trainPosition[1]++
-            break
-
-        default:
-            return 'none'
+    const resultMap = {
+        '*': 'eat',
+        o: 'crash',
+        '@': 'crash',
+        '·': 'none',
+        undefined: 'crash',
+        '|': 'crash',
     }
 
-    const status = checkStatus(trainPosition)
-    return status
+    const vectorizedBoard = board.join('|').toString()
+    const trainIndex = vectorizedBoard.indexOf('@')
+
+    return resultMap[vectorizedBoard[trainIndex + movActions[mov]]]
 }
