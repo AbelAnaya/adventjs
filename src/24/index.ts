457412ebfa3edad03ba85b9ae43903e072efb9b5
AbelAnaya <abelanaya97@gmail.com>
2025-01-14 00:08:44 +0100

    Add challenge 24. 1 star

--- src/24/index.ts
+++ src/24/index.ts
@@ -2,5 +2,10 @@ export default function isTreesSynchronized(
     tree1: { value: string; left?: any; right?: any } | undefined,
     tree2: { value: string; left?: any; right?: any } | undefined
 ): [boolean, string] {
-    return [false, '']
+    const sharedRoot = tree1?.value === tree2?.value
+    let isMirror = tree1?.left?.value === tree2?.right?.value
+    isMirror &&= tree1?.right?.value === tree2?.left?.value
+    isMirror &&= tree1?.right?.right?.value === tree2?.left?.left?.value
+
+    return [sharedRoot && isMirror, String(tree1?.value)]
 }
