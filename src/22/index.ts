b4a757607ab6a190cb11b9f3b2239b1ade16a70a
AbelAnaya <abelanaya97@gmail.com>
2025-01-15 12:57:41 +0100

    Add challenge 22. 5 stars

--- src/22/index.ts
+++ src/22/index.ts
@@ -1,31 +1,42 @@
 export default function generateGiftSets(gifts: string[]): string[][] {
-    const giftSet: string[][] = []
+    const maxCombinations: number = 2 ** gifts.length - 1
+    const combinations: string[][] = []
 
-    gifts.forEach((item) => {
-        giftSet.push([item])
-    })
+    for (const gift of gifts) combinations.push([gift])
 
-    for (let combinations = 1; combinations < gifts.length; ++combinations) {
-        giftSet.forEach((array) => {
-            gifts.forEach((gift) => {
-                const giftSetIteration = new Set(array)
-                giftSetIteration.add(gift)
+    let iStart: number = 0
+    let iStop: number = gifts.length - 1
+    let diff: number = iStop - 1
 
-                const giftArr = Array.from(giftSetIteration)
+    let jStop: number = iStop + 1
 
-                let included = false
-                giftSet.forEach((arr) => {
-                    if (JSON.stringify(arr.toSorted()) === JSON.stringify(giftArr.toSorted())) {
-                        included = true
-                    }
-                })
+    while (combinations.length < maxCombinations - 1) {
+        let offset: number = 0
 
-                if (!included) {
-                    giftSet.push(giftArr)
-                }
-            })
-        })
+        for (let i = iStart; i < iStop; i++) {
+            const anchorGift: string[] = combinations[i]
+
+            for (let j = i + 1; j < jStop; j++) {
+                const nextGift: string[] = combinations[j].filter((value) => !anchorGift.includes(value))
+                combinations.push([...anchorGift, ...nextGift])
+            }
+
+            offset++
+        }
+
+        iStart = jStop
+        iStop += offset
+
+        if (iStart === iStop) {
+            iStart++
+            iStop = iStart + diff - 1
+            diff = iStop - iStart
+        }
+
+        jStop = iStop + 1
     }
 
-    return giftSet
+    combinations.length !== maxCombinations && combinations.push(gifts)
+
+    return combinations
 }
